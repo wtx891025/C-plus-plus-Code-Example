236a9fc07e2d5f17612b0695d769f1410f22be12
wtx891025 <142137639+wtx891025@users.noreply.github.com>
2023-08-19 13:58:20 +0800

    Delete ch1 directory

--- ch1/practice1_3.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream> 
-
-//　寫一個程式在標準輸出印出Hello, World 
-int main(){
-    std::cout << "Hello, World" << std::endl;
-}
--- ch1/practice1_4.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream> 
-
-//寫一個程式使用乘法運算子印出成績
-int main(){
-    int a, b;
-    std::cin >> a >> b;
-    std::cout << a*b << std::endl;
-}
